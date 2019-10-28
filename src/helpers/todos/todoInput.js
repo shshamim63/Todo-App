aa7f0de50f384ab34eafd2b7c68d4be6a641b5d9
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-10-28 21:05:16 +0600

    crete todo basic template converted to dom

--- src/helpers/todos/todoInput.js
+++ src/helpers/todos/todoInput.js
@@ -5,10 +5,16 @@ const todoInput = (() => {
     const description = document.querySelector('#todo-description').value;
     const date = new Date(document.querySelector('#todo-date').value).toDateString();
     const priority = document.querySelector('#todo-priority').value;
-    return {todoId, title, description, date, priority};
+    return {
+      todoId,
+      title,
+      description,
+      date,
+      priority,
+    };
   };
   return {
-    getTodoInfo
+    getTodoInfo,
   };
 })();
 export default todoInput;
--- src/helpers/view/todolistDisplay.js
+++ src/helpers/view/todolistDisplay.js
@@ -2,11 +2,11 @@ const todolistDisplay = (() => {
   const changeCurrentRowView = (targetId, currentStatus) => {
     const todoRows = document.querySelectorAll('tr');
     todoRows.forEach((element) => {
-      elementId = parseInt(element.id.split('-')[1], 10);
+      const elementId = parseInt(element.id.split('-')[1], 10);
       if (elementId === targetId) {
         if (currentStatus === true) {
           todoRows.classList.remove('bg-primary');
-          todoRows.classList.add('bg-success');  
+          todoRows.classList.add('bg-success');
         } else {
           todoRows.classList.remove('bg-primary');
           todoRows.classList.add('bg-primary');
@@ -26,33 +26,75 @@ const todolistDisplay = (() => {
   const createCheckbox = (todo) => {
     const statusCheckboxContainer = document.createElement('td');
     const statusCheckbox = document.createElement('input');
-    statusCheckbox.setAttribute("type", "checkbox");
+    statusCheckbox.setAttribute('type', 'checkbox');
     if (todo.status) {
-      statusCheckbox.setAttribute("checked", true);
-      statusCheckbox.setAttribute("value", todo.status);
+      statusCheckbox.setAttribute('checked', true);
+      statusCheckbox.setAttribute('value', todo.status);
     } else {
-      statusCheckbox.setAttribute("checked", false);
-      statusCheckbox.setAttribute("value", todo.status);
+      statusCheckbox.setAttribute('checked', false);
+      statusCheckbox.setAttribute('value', todo.status);
     }
     statusCheckbox.addEventListener('change', () => {
       todo.status = !todo.status;
       changeCurrentRowView(todo.id, todo.status);
       updateTodo(todo);
     });
+    return statusCheckboxContainer.appendChild(statusCheckbox);
   };
-  const eachTodoListRender = (todo) => {
-    // <tr class="bg-success">
-    //   <td><input type="checkbox" aria-label="Checkbox for following text input"></td>
-    //   <th scope="row">Create a task</th>
-    //   <td>To complete the task</td>
-    //   <td>12-11-2019</td>
-    //   <td>Normal</td>
-    //   <td>
-    //     <img src="./assets/images/delete.png" alt="" class="todo-delete-img">
-    //     <img src="./assets/images/edit.png" alt="" class="todo-edit-img" data-toggle="modal" data-target="#exampleModal3">
-    //   </td>
-    // </tr>
+  const createTodoTitleContainer = (todo) => {
+    const todoTitleContainer = document.createElement('th');
+    todoTitleContainer.setAttribute('scope', 'row');
+    todoTitleContainer.innerText = todo.title;
+    return todoTitleContainer;
+  };
+  const createTodoDescriptionContainer = (todo) => {
+    const todoDescriptionContainer = document.createElement('td');
+    todoDescriptionContainer.innerText = todo.description;
+    return todoDescriptionContainer;
+  };
+  const createTodoDate = (todo) => {
+    const todoDateContainer = document.createElement('td');
+    todoDateContainer.innerText = todo.date;
+    return todoDateContainer;
+  };
+  const createTodoPriority = (todo) => {
+    const todoPriorityContainer = document.createElement('td');
+    todoPriorityContainer.innerText = todo.priority;
+    return todoPriorityContainer;
+  };
+  const createDeleteImage = (targetID) => {
+    const editImageContainer = document.createElement('img');
+    editImageContainer.setAttribute('src', './assets/images/edit.png');
+    editImageContainer.setAttribute('alt', 'edit image');
+    editImageContainer.classList.add('todo-edit-img');
+    editImageContainer.setAttribute('data-toggle', 'modal');
+    editImageContainer.setAttribute('data-target', '#exampleModal3');
+    editImageContainer.addEventListener('click', () => {
+      deleteTodo(targetID);
+      removeFromTodoList(targetID);
+    });
+  };
+  const createEditImage = (target) => {
+    const editImageContainer = document.createElement('img');
+    editImageContainer.setAttribute('src', './assets/images/edit.png');
+    editImageContainer.setAttribute('alt', 'edit image');
+    editImageContainer.classList.add('todo-edit-img');
+    editImageContainer.setAttribute('data-toggle', 'modal');
+    editImageContainer.setAttribute('data-target', '#exampleModal3');
+    loadEditModal(target);
+  };
+  const creteTodoAction = (todo) => {
+    const todoActionContainer = document.createElement('td');
+    const deleteImage = createDeleteImage(todo.id);
+    const editImage = createEditImage(todo);
+  };
+  const renderEachTodoList = (todo) => {
     const tableRow = createTableRow(todo);
     const checkboxElement = createCheckbox(todo);
+    const todoTitle = createTodoTitleContainer(todo);
+    const todoDescription = createTodoDescriptionContainer(todo);
+    const todoDate = createTodoDate(todo);
+    const todoPriority = createTodoPriority(todo);
+    const todoAction = creteTodoAction(todo);
   };
 })();
