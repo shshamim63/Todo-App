59872bd9db61bf9f7b22642af908602b88a43a43
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-11-01 17:13:18 +0600

    Cleaning html template and compressing css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,16 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    main: './src/index.js',
+    vendor: './src/vendor.js',
+  },
   output: {
     filename: 'main.[contentHash].js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: './src/template.html',
-  })],
   module: {
     rules: [
-      {
-        test: /\.scss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
       {
         test: /\.html$/i,
         use: ['html-loader'],
